2254fe05cbb2b2d5048b89f50b74af4c36b9b2cc
RacMcT <raccmct@gmail.com>
2021-12-27 18:45:53 -0600

    server and user controllers

--- scr/server/server.js
+++ scr/server/server.js
@@ -1,5 +1,14 @@
 import express from "express";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express(); // main thing
 
 app.use(express.json()); // to accept json data
+
+// import { errorHandler, notFound } from "./middleware/errorMiddleware.js"; **finalizing middlewear for users and error handling
+
+dotenv.config();
+
+// connectDB(); ** Will use for when I get DB credentials
+
+// app.use("/api/users", userRoutes); ** will use when finish userRoutes and have DB to connect with
