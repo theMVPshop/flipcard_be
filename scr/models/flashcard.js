17cceeb1bde0a0497152cff92851f5a8995f9161
RacMcT <raccmct@gmail.com>
2021-12-28 20:12:00 -0600

    flashcard schema

---
+++ scr/models/flashcard.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+	{
+		//category- should this be a radio button in dropdown?- how to work into schema?
+		category: {
+			type: String,
+			required: true,
+		},
+		//this is published or draft- default is false so default is that it is a draft
+		visible: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+		setName: {
+			type: String,
+			required: true,
+		},
+		front: {
+			type: String,
+			required: true,
+		},
+		back: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Flashcard = mongoose.model("Flashcard", flashcardSchema);
+
+export default Flashcard;
