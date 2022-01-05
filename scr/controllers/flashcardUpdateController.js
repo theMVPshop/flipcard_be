7a9d37f7fe11a0d9ec12c2d3364f71e54b712d9c
RacMcT <raccmct@gmail.com>
2022-01-04 18:15:44 -0600

    adding crud testing for flash controller

--- scr/controllers/flashcardUpdateController.js
+++ scr/controllers/flashcardUpdateController.js
@@ -4,20 +4,22 @@ import Flashcard from "../models/flashcard";
 const updateFlashcardSet = asyncHandler(async (req, res) => {
 	const flashcard = await Flashcard.findById(req.flashcard._id);
 
+	//has to match flashcard set in order to confirm it's the correct one to update and pull/update the data?
+
 	if (flashcard) {
-		flashcard.name = req.body.name || flashcard.name;
-		flashcard.email = req.body.email || flashcard.email;
-		if (req.body.password) {
-			user.password = req.body.password;
-		}
+		flashcard.setName = req.body.setName || flashcard.setName;
 
 		const updatedFlashcardSet = await flashcard.save();
 
 		res.json({
 			_id: updatedFlashcardSet._id,
-			name: updatedFlashcardSet.name,
-			email: updatedFlashcardSet.email,
-			token: generateToken(updatedFlashcardSet._id),
+			category: updatedFlashcardSet.category,
+			visible: updatedFlashcardSet.visible,
+			setName: updatedFlashcardSet.setName,
+			description: updatedFlashcardSet.description,
+			term: updatedFlashcardSet.term,
+			definition: updatedFlashcardSet.definition,
+			imageUrl: updatedFlashcardSet.imageUrl,
 		});
 	} else {
 		res.status(404);
