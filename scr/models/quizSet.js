716cd92d83d01e8a8906d73dfbcfe45791e2297f
RacMcT <raccmct@gmail.com>
2022-01-05 20:27:07 -0600

    quiz schema drafts

--- scr/models/quizSet.js
+++ scr/models/quizSet.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const flashcardSchema = mongoose.Schema(
+const quizSetSchema = mongoose.Schema(
 	{
-		//category- should this be a single enummerated value from dropdown- this is the course- dental, medical, code- pulls from flashcard
+		//category- should this be a single enummerated value from dropdown- this is the course- dental, medical, code
 		category: {
 			type: String,
-			enum: ["Dental", "Medical", "Coding"], //need to check same syntax as FE dropdowns when they build & from flashcard schema
+			enum: ["Dental", "Medical", "Coding"], //need to check same syntax as FE dropdowns when they build
 			required: true,
 		},
 		//this is published or draft- default is false so default is that it is a draft- need to check with FE for how putting into form- radio, toggle or reg button
@@ -14,21 +14,21 @@ const flashcardSchema = mongoose.Schema(
 			required: true,
 			default: false,
 		},
-		quizName: {
+		setName: {
 			type: String,
 			required: true,
 		},
-		//description is option text filed in FE form- pulls from the flashcard "set/collection"- just push for form edits and review, not pushed in individual cards
+		//description is option text filed in FE form- for the flashcard "set/collection"- just push for form edits and review, not pushed in individual cards
 		description: {
 			type: String,
 			required: false,
 		},
-		//term & definition- front/back of flashcards- the correct answer & question need to be pulled & add in extra questions
-		questions: {
+		//term & definition- front/back of flashcards
+		term: {
 			type: String,
 			required: true,
 		},
-		answer: {
+		definition: {
 			type: String,
 			required: true,
 		},
