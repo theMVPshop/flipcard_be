ff611755dbb2691e0952698a06abfcf760bdb28f
Pace Davis <pacemdavis@gmail.com>
2022-02-16 18:56:16 -0600

    db connected

--- scr/db/database.js
+++ scr/db/database.js
@@ -1,16 +1,45 @@
 //Connection file to mongo db
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-		});
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-		process.exit();
-	}
-};
-export default connectDB;
+// const connectDB = async () => {
+// 	try {
+// 		const conn = await mongoose.connect(process.env.MONGO_URI, {
+// 			useUnifiedTopology: true,
+// 			useNewUrlParser: true,
+// 		});
+// 		console.log(`MongoDB Connected: ${conn.connection.host}`);
+// 	} catch (error) {
+// 		console.error(`Error: ${error.message}`);
+// 		process.exit();
+// 	}
+// };
+// export default connectDB;
+
+
+//Connect file to mysql db
+
+onst mysql = require('mysql')
+require('dotenv').config();
+
+class Connection {
+  constructor() {
+    if (!this.pool) {
+      console.log('creating connection...')
+      this.pool =mysql.createPool({
+        connectionLimit: 100,
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'bbcc9651044654',
+        password: process.env.PASSPHRASE,
+        database: 'heroku_9ed20083dd1d7c7'
+      })
+
+      return this.pool
+    }
+
+    return this.pool
+  }
+}
+
+const instance = new Connection()
+
+module.exports = instance;
--- scr/server.js
+++ scr/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./db/database.js";
+// import connectDB from "./db/database.js";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
@@ -25,15 +25,15 @@ app.get("/", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-// app.listen(
-// 	PORT,
-// 	console.log(
-// 		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`
-// 	)
-// );
+app.listen(
+	PORT,
+	console.log(
+		`Server running on port ${PORT}..`, 
+	)
+);
 
-app.listen(8080, () => {
-	console.log("Application listening on port 8080!");
-});
+// app.listen(8080, () => {
+// 	console.log("Application listening on port 8080!");
+// });
