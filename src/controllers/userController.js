16bb1e085fe32ea4db9c3565b0a4f247007ec9f0
michaelscoggins <rhythmghosts@gmail.com>
2022-03-28 06:42:48 -0500

    commented userController functions for clarity and readability

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,6 @@ import mysql from "mysql"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import pool from "../db/database.js"
-import AppError from "../utils/AppError.js"
 import { promisify } from "util"
 
 //turns "pool.query()" into a promise so that we can use async/await instead of callbacks
@@ -26,23 +25,26 @@ const saltRounds = 10
 
 //signup
 const registerUser = async (req, res, next) =>
-  !req.body.email || !req.body.password
-    ? res.status(400).json({ msg: "Please enter an email and password" })
-    : (await poolQuery(
+  !req.body.email || !req.body.password //check for email and password
+    ? res.status(400).json({ msg: "Please enter an email and password" }) //if not present, send error
+    : //if email and password are present, create connection to the database
+    (await poolQuery(
+        //format query to insert user into database
         mysql.format(
-          "INSERT INTO users (email, password, first_name, last_name, program) VALUES (?,?,?,?,?)",
+          "INSERT INTO users (email, password, first_name, last_name, program) VALUES (?,?,?,?,?)", //insert user into database
           [
             req.body.email,
-            await bcrypt.hash(req.body.password, saltRounds),
+            await bcrypt.hash(req.body.password, saltRounds), //hash password
             req.body.first_name,
             req.body.last_name,
             req.body.program,
-          ]
+          ] //the values which replace the ?s in the query
         )
       ))
-    ? res.status(201).json({ msg: "Your email has been registered!" })
-    : next(new AppError("Something went wrong", 500))
+    ? res.status(201).json({ msg: "Your email has been registered!" }) //if successful, send success message
+    : res.status(400).json({ msg: "Something went wrong" }) //if not, send error
 
+// alternate registerUser function
 // const registerUser = async (req, res, next) => {
 //   if (!req.body.email || !req.body.password)
 //     return next(new AppError("Please provide an email and password", 400))
@@ -60,21 +62,21 @@ const registerUser = async (req, res, next) =>
 //   return res.status(201).json({ msg: "Your email has been registered!" })
 // }
 
-//check password and login
+//check password and then login
 const authUser = async (req, res, next) => {
-  const getUser = "SELECT * FROM users WHERE LOWER(email) = LOWER(?)"
-  let user, storedHash, authenticated, token
-  user = await poolQuery(mysql.format(getUser, req.body.email))
-  !user.length && res.status(400).json({ msg: "Invalid email or password" })
-  storedHash = user[0].password
-  authenticated = await bcrypt.compare(req.body.password, storedHash)
+  const getUser = "SELECT * FROM users WHERE LOWER(email) = LOWER(?)" //LOWER() is used to make email case insensitive
+  let user, storedHash, authenticated, token //storedHash is the hashed password from the database
+  user = await poolQuery(mysql.format(getUser, req.body.email)) //get user from database
+  !user.length && res.status(400).json({ msg: "Invalid email or password" }) //send error if user doesn't exist
+  storedHash = user[0].password //get hashed password from database
+  authenticated = await bcrypt.compare(req.body.password, storedHash) //compare hashed password from database with hashed password from user
   token =
-    authenticated &&
-    jwt.sign({ id: user[0].user_id }, "the-super-strong-secret", { expiresIn: "1h" })
+    authenticated && //if authenticated/passwords match
+    jwt.sign({ id: user[0].user_id }, "the-super-strong-secret", { expiresIn: "1h" }) //generate token
 
-  authenticated
-    ? res.status(200).json({ msg: "Logged in!", token, user: user[0] })
-    : res.status(403).json({ msg: "Invalid email or password)" })
+  authenticated //if authenticated
+    ? res.status(200).json({ msg: "Logged in!", token, user: user[0] }) //send token and user
+    : res.status(403).json({ msg: "Invalid email or password)" }) //if not authenticated
 }
 
 export { registerUser, getAllUsers, getUserById, authUser }
