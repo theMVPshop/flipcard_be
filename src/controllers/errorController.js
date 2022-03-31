b97f683bb711bb6496e327ed69c28644af7c0e96
michaelscoggins <rhythmghosts@gmail.com>
2022-03-31 04:00:25 -0500

    quick fix on console.log arg

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,11 +1,11 @@
 const sendError = (err, res) => {
-  let logged = res.status(err.statusCode || 500).json({
+  res.status(err.statusCode || 500).json({
     status: err.status || "error",
     error: err,
     message: err.message,
     stack: err.stack,
   })
-  console.log("hey michael", logged)
+  console.log("hey michael", err)
 }
 
 export default (err, req, res, next) => sendError(err, res)
