83b8caf88ac94dbd02fc0a93cf2c471583cedf5c
michaelscoggins <rhythmghosts@gmail.com>
2022-03-31 03:58:01 -0500

    added console logging for errors to aid in browser debugging for a little while

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,9 +1,11 @@
-const sendError = (err, res) =>
-  res.status(err.statusCode || 500).json({
+const sendError = (err, res) => {
+  let logged = res.status(err.statusCode || 500).json({
     status: err.status || "error",
     error: err,
     message: err.message,
     stack: err.stack,
   })
+  console.log("hey michael", logged)
+}
 
 export default (err, req, res, next) => sendError(err, res)
