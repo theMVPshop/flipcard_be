d04c91c4f5051ad48831af932337342b19c03d45
Sam Dunegan <cesarcisneros9@gmail.com>
2022-04-12 19:22:46 -0500

    added a try catch block to catch any unexpected error with JWT token and end the connection.

--- src/utils/checkJwt.js
+++ src/utils/checkJwt.js
@@ -1,21 +1,22 @@
-import { promisify } from "util"
 import jwt from "jsonwebtoken"
 
 export default async (req, res, next) => {
-    console.log(req.header.authorization)
   let token
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     token = req.headers.authorization.split(" ")[1]
   }
   if (!token) {
-    return res.status(403).json({ msg: "You are not logged in, please log in and try again.)" })
+    return res.status(403).json({ msg: "You are not logged in, please log in and try again." })
   }
-
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
-
+  let decoded
+  try {
+    decoded = await jwt.verify(token, process.env.JWT_SECRET)
+  } catch (err) {
+    console.log("err: ", err)
+    return res.status(500).json({ status: "error", msg: "Problem with JWT token", error: err })
+  }
   // GRANT ACCESS TO PROTECTED ROUTE!
   req.user_id = decoded.user_id
 
   next()
 }
-
